6eb67ae1f94001ba448cd1ab2fa0970d4d206ee5
mana <mnm050125@gmail.com>
2024-07-08 14:09:19 +0900

    Fix typo in station13.js title

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -2,10 +2,13 @@ data = "global data";
 
 // このreturnDataに代入される関数の形をアロー関数に変えて"global data"が表示されるようにしてください
 // ↓関数の中の処理は変更しないでください
-const returnThisData = function () {
-  return this.data;
-};
+// const returnThisData = function () {
+  // return this.data;
+// };
 // ↑関数の中の処理は変更しないでください
+const returnThisData = () => {
+  return this.data;
+}; //書き換えたやつ
 
 const object = {
   data: "object data",
@@ -15,3 +18,4 @@ const object = {
 function getData() {
   return object.func();
 }
+
